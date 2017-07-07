faf5f5186471596016c0d624d2e4db83d98e74ca
neoFelhz <neofelhz@gmail.com>
2017-07-07 15:44:08 +0800

    Minor changes Build related:   - use gulp to build the file Project related:   - fix the width   - add some meta tags

---
+++ README.md
@@ -0,0 +1,49 @@
+# STFW.info
+
+The repo of [stfw.info](https://stfw.info)
+
+## Development
+
+### Requirement
+
+- Node.JS
+- Gulp
+- Project Dependenciess
+  - gulp-htmlmin
+  - gulp-htmlclean
+  - gulp-uncss
+
+```bash
+npm install gulp -g
+npm install
+```
+
+### Running
+
+Simply run in project root directory:
+
+```
+gulp
+```
+
+The html will automatically output in 'public/' and the minified css will automatically output in `public/css/msui.min.css`.
+The gulp webserver will listen on `http://localhost:8080`. The broswer will automatically open the html.
+
+### Project Structure
+
+Directory/File | Description |
+----------| ------------|
+Public         | Production Environment |
+ ├── fonts | Roboto Fonts |
+ ├── icons     | Material Design Icons |
+ ├── js     | `mdui.min.js` |
+source         | Raw project |
+ ├── css | The raw css without minified |
+ ├── index.html | The raw html file without minified |
+gulpfile.js  | Gulp tasks definition |
+package.json | Package Information |
+
+## Todo
+
+- Using Travis.CI to make the building more easily.
+
---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var htmlclean = require('gulp-htmlclean');
+var webserver = require('gulp-webserver');
+var uncss = require('gulp-uncss');
+gulp.task('minify-html', function () {
+    return gulp.src('source/index.html')
+        .pipe(htmlmin({
+            removeComments: true,
+            minifyJS: true,
+            minifyCSS: true,
+            collapseWhitespace: true,
+        }))
+        .pipe(htmlclean())
+        .pipe(gulp.dest('public'))
+});
+gulp.task('uncss', function () {
+    return gulp.src('./source/css/mdui.min.css')
+        .pipe(uncss({
+            html: ['public/index.html']
+        }))
+        .pipe(gulp.dest('public/css'));
+});
+gulp.task('webserver', function () {
+    gulp.src('./public/')
+        .pipe(webserver({
+            host: 'localhost',
+            livereload: false,
+            directoryListing: true,
+            open: '/index.html',
+            port: 8080
+        }));
+});
+gulp.task('default', [
+    'minify-html',
+    'uncss',
+    'webserver'
+]);
