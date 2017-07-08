0147939697ca42b644321f24fdc194aae895167b
Rachel030219 <291054446@qq.com>
2017-07-08 09:29:55 +0800

    added test task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
+var webserver = require('gulp-webserver');
+var uncss = require('gulp-uncss');
 gulp.task('minify-html', function () {
     return gulp.src('source/index.html')
         .pipe(htmlmin({
@@ -12,6 +14,27 @@ gulp.task('minify-html', function () {
         .pipe(htmlclean())
         .pipe(gulp.dest('public'))
 });
+gulp.task('uncss', function () {
+    return gulp.src('./source/css/mdui.min.css')
+        .pipe(uncss({
+            html: ['public/index.html']
+        }))
+        .pipe(gulp.dest('public/css'));
+});
+gulp.task('webserver', function () {
+    gulp.src('./public/')
+        .pipe(webserver({
+            host: 'localhost',
+            livereload: false,
+            directoryListing: true,
+            open: '/index.html',
+            port: 8080
+        }));
+});
 gulp.task('default', [
+    'minify-html',
+    'webserver'
+]);
+gulp.task('test', [
     'minify-html'
 ]);
