7eefbeb960453577adf552e6b64281fdd0071f2c
Rachel030219 <tangrui003@gmail.com>
2019-06-05 22:46:59 +0800

    updated

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
 var webserver = require('gulp-webserver');
-var uncss = require('gulp-uncss');
 gulp.task('minify-html', function () {
     return gulp.src('source/index.html')
         .pipe(htmlmin({
@@ -14,27 +13,15 @@ gulp.task('minify-html', function () {
         .pipe(htmlclean())
         .pipe(gulp.dest('public'))
 });
-gulp.task('uncss', function () {
-    return gulp.src('./source/css/mdui.min.css')
-        .pipe(uncss({
-            html: ['public/index.html']
-        }))
-        .pipe(gulp.dest('public/css'));
-});
 gulp.task('webserver', function () {
     gulp.src('./public/')
         .pipe(webserver({
             host: 'localhost',
             livereload: false,
-            directoryListing: true,
-            open: '/index.html',
+            directoryListing: false,
+            open: 'index.html',
             port: 8080
         }));
 });
-gulp.task('default', [
-    'minify-html',
-    'webserver'
-]);
-gulp.task('build', [
-    'minify-html'
-]);
+gulp.task('build', gulp.series('minify-html'));
+gulp.task('default', gulp.series('build', 'webserver'));
